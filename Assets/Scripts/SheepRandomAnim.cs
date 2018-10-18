d2d2afafa72bf355ad4a041b44dfc61d27557f4f
phenixapoil <35372381+phenixapoil@users.noreply.github.com>
2018-10-18 12:39:49 +0200

    Sheeps can die

---
+++ Assets/Scripts/SheepRandomAnim.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SheepRandomAnim : MonoBehaviour {
+    private Animator anim;
+	// Use this for initialization
+
+	void Start () {
+        anim = this.GetComponent<Animator>();
+        anim.speed = Random.Range(0.7f, 1.3f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
