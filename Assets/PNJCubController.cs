a847bd96fd03c28fcbca29fee19d3d6e9ba99faa
phenixapoil <35372381+phenixapoil@users.noreply.github.com>
2018-10-18 09:33:21 +0200

    bearcubanimation

--- Assets/PNJCubController.cs
+++ Assets/PNJCubController.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PNJCubController : MonoBehaviour {
     public GameObject TargetPlayer;
     private Animator myAnimator;
-    private bool is_awake = false;
+    private bool is_awake;
     private Rigidbody2D rigidBody;
     private float distancePlayer;
     public float follow_distance;
@@ -14,6 +14,7 @@ public class PNJCubController : MonoBehaviour {
     void Start() {
         myAnimator = GetComponent<Animator>();
         rigidBody = GetComponent<Rigidbody2D>();
+        is_awake = false;
     }
 
     // Update is called once per frame
@@ -21,7 +22,7 @@ public class PNJCubController : MonoBehaviour {
         distancePlayer = TargetPlayer.transform.position.x - transform.position.x;
         if (is_awake && Mathf.Abs(distancePlayer) > follow_distance)
         {
-            rigidBody.velocity = new Vector2(Mathf.Min(distancePlayer / 10, 1) * speed, 0);
+            rigidBody.velocity = new Vector2(Mathf.Min(distancePlayer/10 , 1) * speed, 0);
             myAnimator.SetBool("Walking",true);
         }
         else
