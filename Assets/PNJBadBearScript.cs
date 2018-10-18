973867741ce4ee2a4f757288d86edc04fc25315e
phenixapoil <35372381+phenixapoil@users.noreply.github.com>
2018-10-18 15:47:27 +0200

    Niveau 2

---
+++ Assets/PNJBadBearScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PNJBadBearScript : MonoBehaviour {
+    public float speed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsName("BadBearWalk"))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-speed, 0);
+        }
+		
+        if (GetComponent<Transform>().position.x < 48)
+        {
+            this.gameObject.SetActive(false);
+        }
+	}
+}
